2c539947bc47bd012cbee824f9ea1e5beb68e1de
DylanGrahamHart <dylanhart.net@gmail.com>
2019-05-28 22:23:36 -0500

    css rebuild working

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,22 @@ const gulp = require('gulp');
 ////
 
 const HTML_SRC = 'src/main/resources/templates/**/*.html';
-
 function html() {
   return gulp.src(HTML_SRC)
     .pipe(gulp.dest('out/production/resources/templates'));
 }
-
 function htmlWatch() {
   gulp.watch(HTML_SRC, html);
 }
 
 const CSS_SRC = 'src/main/resources/static/**/*.css';
-
-function html() {
+function css() {
   return gulp.src(CSS_SRC)
     .pipe(gulp.dest('out/production/resources/static'));
 }
-
-function htmlWatch() {
-  gulp.watch(CSS_SRC, html);
+function cssWatch() {
+  gulp.watch(CSS_SRC, css);
 }
 
-exports.default = gulp.series(htmlWatch, html);
+exports.default = gulp.parallel(cssWatch, htmlWatch, css, html);
 
