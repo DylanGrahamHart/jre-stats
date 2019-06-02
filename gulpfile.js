2c10f894c3e4da0e0b062bf7cd03fcb5eb8f9db3
DylanGrahamHart <dylanhart.net@gmail.com>
2019-06-02 16:13:56 -0500

    adding scss and cleaning up styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 ////
 
@@ -11,14 +13,24 @@ function htmlWatch() {
   gulp.watch(HTML_SRC, html);
 }
 
-const CSS_SRC = 'src/main/resources/static/**/*.css';
-function css() {
+const CSS_SRC = 'src/main/resources/static/**/*.scss';
+function css1() {
   return gulp.src(CSS_SRC)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('out/production/resources/static'));
 }
+function css2() {
+  return gulp.src(CSS_SRC)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('src/main/resources/static'));
+}
 function cssWatch() {
-  gulp.watch(CSS_SRC, css);
+  gulp.watch(CSS_SRC, gulp.parallel(css1, css2));
 }
 
-exports.default = gulp.parallel(cssWatch, htmlWatch, css, html);
+exports.default = gulp.parallel(cssWatch, htmlWatch, css1, css2, html);
 
