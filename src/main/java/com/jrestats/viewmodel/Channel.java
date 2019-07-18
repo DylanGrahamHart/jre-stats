652f8f34dfef48b35e4c1ba07ebac5545d598f7b
DylanGrahamHart <dylanhart.net@gmail.com>
2019-07-17 21:49:33 -0500

    adding img

--- src/main/java/com/jrestats/viewmodel/Channel.java
+++ src/main/java/com/jrestats/viewmodel/Channel.java
@@ -10,15 +10,22 @@ public class Channel {
 
     public String subscriberCount;
     public String viewCount;
+    public String imgSrc;
 
     public Channel(Map<String, Object> channelsResponse) {
-        Map<String, Object> statistics = JreUtil.getMap(
-                "statistics",
-                JreUtil.getList("items", channelsResponse).get(0)
-        );
+        Map<String, Object> item = JreUtil.getList("items", channelsResponse).get(0);
+        Map<String, Object> statistics = JreUtil.getMap("statistics", item);
+        Map<String, Object> snippet = JreUtil.getMap("snippet", item);
 
         setSubscriberCount(statistics);
         setViewCount(statistics);
+        setImgSrc(snippet);
+    }
+
+    private void setImgSrc(Map<String, Object> snippet) {
+        Map<String, Object> thumbnails = JreUtil.getMap("thumbnails", snippet);
+        Map<String, Object> img = JreUtil.getMap("medium", thumbnails);
+        imgSrc = (String) img.get("url");
     }
 
     public void setSubscriberCount(Map<String, Object> statistics) {
@@ -26,6 +33,7 @@ public class Channel {
                 Integer.parseInt((String) statistics.get("subscriberCount"))
         );
     }
+
     public void setViewCount(Map<String, Object> statistics) {
         this.viewCount = formatNumber(
                 Integer.parseInt((String) statistics.get("viewCount"))
