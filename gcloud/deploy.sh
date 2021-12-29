3b7b767eb73a278ab269c7403fc8937e37f31049
DylanGrahamHart <dylanhart.net@gmail.com>
2021-12-29 11:26:59 -0600

    idk

--- gcloud/deploy.sh
+++ gcloud/deploy.sh
@@ -1,5 +1,7 @@
 ./gradlew build
 gsutil cp build/libs/*.jar gs://jre-stats-jars/derp.jar
+
+gcloud config set project jre-stats-242504
 gcloud compute instances stop instance-1
 gcloud compute instances start instance-1
 
